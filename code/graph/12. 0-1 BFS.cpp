dd7765eb3217e25424f327954274c38f0ce505e1
Istiaque_ahmed <istiaqueahmedarik@gmail.com>
2025-11-20 00:24:39 +0600

    3

---
+++ code/graph/12. 0-1 BFS.cpp
@@ -0,0 +1,11 @@
+vector<ll> d(n, INF);d[s] = 0;deque<ll> q; q.push_front(s);
+while (!q.empty()) {
+    ll v = q.front(); q.pop_front();
+    for (auto edge : adj[v]) {
+        ll u = edge.first;
+        ll w = edge.second;
+        if (d[v] + w < d[u]) {
+            d[u] = d[v] + w;
+   if (w == 1)  q.push_back(u);
+   else q.push_front(u);
+   }}}
